2dddbb700b0ba3441929430275d8ae5b5540d2df
jose <jose.s.contacto@gmail.com>
2024-01-16 18:39:15 -0300

    Test(study note reciever): Only acepts lists with strings

--- tests/test_study_notes_reciever.py
+++ tests/test_study_notes_reciever.py
@@ -16,3 +16,11 @@ def test_sut_shouldnt_accept_nothing_but_list():
     sut = StudyNoteReciever()
     with pytest.raises(ValueError):
         sut.recieve_notes(notes)
+
+def test_sut_shouldnt_accept_list_with_nothing_but_string():
+    notes = [1,2,3,4]
+    sut = StudyNoteReciever()
+    with pytest.raises(ValueError):
+        sut.recieve_notes(notes)
+
+
