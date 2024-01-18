e790a55f90d0cc8fc6a811b2105185fd49220052
jose <jose.s.contacto@gmail.com>
2024-01-18 11:37:07 -0300

    Test(recievernotedto): has content and tags

---
+++ tests/test_dtos/test_note_dto.py
@@ -0,0 +1,8 @@
+from dtos.recievenote import RecieveNoteDTO
+def test_dto_has_content_and_tags():
+    sut = RecieveNoteDTO(content="content",
+                         tags=['tag1','tag2'])
+    sut_response_1 = sut.get_content()
+    sut_response_2 = sut.get_tags()
+    assert sut_response_1 == 'content'
+    assert sut_response_2 == ['tag1','tag2']
