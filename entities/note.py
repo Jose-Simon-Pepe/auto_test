6a6c283c696dbd8c11eea9c211bc7415311adf29
jose <jose.s.contacto@gmail.com>
2024-01-18 11:28:33 -0300

    Test(note): Note has tags

--- entities/note.py
+++ entities/note.py
@@ -1,8 +1,13 @@
 class Note:
-    def __init__(self,content:str=None):
+    def __init__(self,content:str=None,tags:list=None):
         if content is None or content.rstrip()=="":
             raise VoidContentNoteException()
         self._content = content
+        self._tags = tags
+
+    def get_tags(self):
+        return self._tags
+
 
 class VoidContentNoteException(BaseException):
     pass
--- tests/test_enities/test_note.py
+++ tests/test_enities/test_note.py
@@ -1,9 +1,13 @@
 import pytest
 from entities.note import *
 
-#NOTE: Note should has also taggs
 
 def test_note_should_has_a_content_not_null():
     content = ""
     with pytest.raises(VoidContentNoteException):
         Note(content=content)
+
+def test_note_should_has_tags():
+    sut = Note(content="Dommie",tags=['tag1','tag2'])
+    sut_response = sut.get_tags()
+    assert sut_response == ['tag1','tag2']
