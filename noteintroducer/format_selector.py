deda382ed1f7ce5eb3af155b0f8ae4f3a6a29366
jose <jose.s.contacto@gmail.com>
2024-01-20 19:03:24 -0300

    test(format selector): load all supported formats up on start

--- noteintroducer/format_selector.py
+++ noteintroducer/format_selector.py
@@ -1,8 +1,10 @@
 class FormatSelector:
     def __init__(self,format_storage=None):
-        self._format_storage= format_storage
-        self._supported_format= ['format1','format2']
+        self._load_supported_formats(format_storage=format_storage)
 
     def get_all_format(self):
         return self._supported_format
+
+    def _load_supported_formats(self,format_storage=None):
+        self._supported_format = format_storage.get_all()
 
--- tests/test_format_selector/test_format_selector.py
+++ tests/test_format_selector/test_format_selector.py
@@ -1,10 +1,21 @@
 from noteintroducer.format_selector import FormatSelector
 """Format Selector Component"""
 
+class FormatStorageStub:
+    def __init__(self):
+        self.supported_formats = ['format1','format2']
+    def get_all(self):
+        return self.supported_formats
+
+
+format_storage_stub = FormatStorageStub()
 
 
 def test_sut_should_get_all_supported_formats():
     supported_formats = ['format1','format2']
-    sut = FormatSelector()
+    sut = FormatSelector(format_storage=format_storage_stub)
     assert sut.get_all_format() == supported_formats
     
+
+def test_sut_should_load_all_supported_formats_up_on_start():
+    assert FormatSelector(format_storage_stub).get_all_format() is not None
