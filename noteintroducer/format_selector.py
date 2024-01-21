f6b9a5c4f527dc0a1fea0490fb47f2a505ebd6a8
jose <jose.s.contacto@gmail.com>
2024-01-20 21:30:46 -0300

    test(format selector): allow user to select attribute format

--- noteintroducer/format_selector.py
+++ noteintroducer/format_selector.py
@@ -1,5 +1,7 @@
 class FormatSelector:
-    def __init__(self,format_storage=None):
+    def __init__(self,format_storage=None,
+                 note_reader_config=None):
+        self._note_reader_config = note_reader_config
         self._load_supported_formats(format_storage=format_storage)
 
     def get_all_format(self):
@@ -7,4 +9,7 @@ class FormatSelector:
 
     def _load_supported_formats(self,format_storage=None):
         self._supported_format = format_storage.get_all()
+
+    def select_expected_note_format(self,expected:str=None):
+        self._note_reader_config.set_expected_note_format(format=expected)
 
--- tests/test_format_selector/test_format_selector.py
+++ tests/test_format_selector/test_format_selector.py
@@ -29,3 +29,25 @@ def test_sut_should_get_all_formats_from_memory_storage():
     memory_storage = MemorySupportedFormat(storage=supported_formats)
     sut = FormatSelector(format_storage=memory_storage)
     assert sut.get_all_format() == supported_formats
+
+class NoteReaderConfigSpy:
+    def __init__(self):
+        self._expected = None
+
+    def expected_note_format(self):
+        return self._expected
+
+
+    def set_expected_note_format(self,format=None):
+        self._expected = format
+
+def test_sut_should_allow_user_to_select_attribute_format():
+    note_reader_config = NoteReaderConfigSpy()
+    sut = FormatSelector(format_storage=format_storage_stub,
+                         note_reader_config=note_reader_config)
+    sut.select_expected_note_format(expected='format1')
+    assert note_reader_config.expected_note_format()=='format1'
+
+
+    
+
