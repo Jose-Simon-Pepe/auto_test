566843e938809549faf613f56a99c78637969814
jose <jose.s.contacto@gmail.com>
2024-01-21 10:59:48 -0300

    Refactor(tests): Use format selector factory

---
+++ tests/helpers/format_selector_factory.py
@@ -0,0 +1,7 @@
+from tests.helpers.format_memory_factory import FormatMemoryFactory
+from noteintroducer.format_selector import FormatSelector
+class FormatSelectorFactory:
+    def __init__(self,note_reader_config=None):
+        self._note_reader_config = note_reader_config
+        self.format_selector = FormatSelector(format_storage=FormatMemoryFactory().memory_supported_format,
+                                     note_reader_config=note_reader_config)
--- tests/test_note_reader/test_note_reader.py
+++ tests/test_note_reader/test_note_reader.py
@@ -1,4 +1,5 @@
 from noteintroducer.format_selector import FormatSelector
+from tests.helpers.format_selector_factory import FormatSelectorFactory
 from tests.helpers.format_memory_factory import FormatMemoryFactory
 from noteintroducer.agents.memory_supported_formats_storage import MemorySupportedFormat
 memory_supported_format = FormatMemoryFactory().memory_supported_format
@@ -26,6 +27,7 @@ def test_note_reader_should_load_config_at_start():
     sut = NoteReader(config=spy_note_config)
     assert spy_note_config._value
 
+
 #NOTE: Integration test
 #NOTE: Crear factory de format selector
 #NOTE: Crear fichero de donde memory storage toma los tipos de formato admitidos
@@ -33,8 +35,7 @@ def test_note_reader_should_load_config_at_start():
 def test_sut_should_read_expected_note_format():
     note_reader_config =  NoteReaderConfigSpy()
     format_storage = memory_supported_format
-    format_selector = FormatSelector(format_storage=format_storage,
-                                     note_reader_config=note_reader_config)
+    format_selector = FormatSelectorFactory(note_reader_config=note_reader_config).format_selector
     format_selector.select_expected_note_format(expected='format1')
     sut = NoteReader(config=note_reader_config)
     assert sut.get_config()['format_expected']['name']=='format1'
